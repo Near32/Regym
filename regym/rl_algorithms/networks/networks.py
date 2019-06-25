f44ffe6b9a4394bdd6ec4c5921ab10a532d1a0cb
Sarios <danielhp95@gmail.com>
2019-06-25 12:31:43 +0100

    PEP8 reformating

--- regym/rl_algorithms/networks/networks.py
+++ regym/rl_algorithms/networks/networks.py
@@ -103,104 +103,93 @@ class DuelingDQN(nn.Module) :
         return x
 
 
-class ActorNN(nn.Module) :
-    def __init__(self,state_dim=3,action_dim=2,action_scaler=1.0,HER=False,actfn=LeakyReLU, use_cuda=False ) :
-        super(ActorNN,self).__init__()
+class ActorNN(nn.Module):
+    def __init__(self, state_dim=3, action_dim=2, action_scaler=1.0, HER=False, actfn=LeakyReLU, use_cuda=False):
+        super(ActorNN, self).__init__()
 
         self.state_dim = state_dim
         self.action_dim = action_dim
         self.action_scaler = action_scaler
 
         self.HER = HER
-        if self.HER :
+        if self.HER:
             self.state_dim *= 2
 
         self.actfn = actfn
-        #Features :
-        self.featx = nn.Linear(self.state_dim,400)
+        # Features:
+        self.featx = nn.Linear(self.state_dim, 400)
         self.featx.weight.data = init_weights(self.featx.weight.data.size())
 
-        # Actor network :
-        self.actor1 = nn.Linear(400,300)
-        self.actor1.weight.data.uniform_(-EPS,EPS)
-        self.actor2 = nn.Linear(300,self.action_dim)
-        self.actor2.weight.data.uniform_(-EPS,EPS)
+        # Actor network:
+        self.actor1 = nn.Linear(400, 300)
+        self.actor1.weight.data.uniform_(-EPS, EPS)
+        self.actor2 = nn.Linear(300, self.action_dim)
+        self.actor2.weight.data.uniform_(-EPS, EPS)
 
         self.use_cuda = use_cuda
-        if self.use_cuda :
+        if self.use_cuda:
             self = self.cuda()
 
-
-    def features(self,x) :
-        fx = self.actfn( self.featx( x) )
+    def forward(self, x):
+        fx  = self.actfn(self.featx(x))
         # batch x 400
-        return fx
-
-    def forward(self, x) :
-        fx = self.features(x)
-        # batch x 400
-        out = self.actfn( self.actor1( fx ) )
+        out = self.actfn(self.actor1(fx))
         # batch x 300
-        out = self.actor2( fx )
+        out = self.actor2(out)
         # batch x self.action_dim
 
-        #scale the actions :
-        unscaled = F.tanh(xx)
+        # scale the actions:
+        unscaled = F.tanh(out)
         scaled = unscaled * self.action_scaler
         return scaled
 
     def clone(self):
-        cloned = ActorNN(state_dim=self.state_dim,action_dim=self.action_dim,action_scaler=self.action_scaler,CNN=self.CNN,HER=self.HER,actfn=self.actfn, use_cuda=self.use_cuda)
-        cloned.load_state_dict( self.state_dict() )
+        cloned = ActorNN(state_dim=self.state_dim, action_dim=self.action_dim, action_scaler=self.action_scaler, CNN=self.CNN, HER=self.HER, actfn=self.actfn, use_cuda=self.use_cuda)
+        cloned.load_state_dict(self.state_dict())
         return cloned
 
-class CriticNN(nn.Module) :
-    def __init__(self,state_dim=3,action_dim=2,HER=False,actfn=LeakyReLU, use_cuda=False  ) :
-        super(CriticNN,self).__init__()
+
+class CriticNN(nn.Module):
+    def __init__(self, state_dim=3, action_dim=2, HER=False, actfn=LeakyReLU, use_cuda=False):
+        super(CriticNN, self).__init__()
 
         self.state_dim = state_dim
         self.action_dim = action_dim
 
         self.HER = HER
-        if self.HER :
+        if self.HER:
             self.state_dim *= 2
 
         self.actfn = actfn
 
-        #Features :
-        self.featx = nn.Linear(self.state_dim,400)
+        # Features:
+        self.featx = nn.Linear(self.state_dim, 400)
         self.featx.weight.data = init_weights(self.featx.weight.data.size())
 
-        # Critic network :
-        ## state value path :
-        self.critic1 = nn.Linear(400+self.action_dim,300)
+        # Critic network:
+        # state value path:
+        self.critic1 = nn.Linear(400+self.action_dim, 300)
         self.critic1.weight.data = init_weights(self.critic1.weight.data.size())
 
-        self.critic2 = nn.Linear(300,1)
-        self.critic2.weight.data.uniform_(-EPS*1e-1,EPS*1e-1)
+        self.critic2 = nn.Linear(300, 1)
+        self.critic2.weight.data.uniform_(-EPS*1e-1, EPS*1e-1)
 
         self.use_cuda = use_cuda
-        if self.use_cuda :
+        if self.use_cuda:
             self = self.cuda()
 
-    def features(self,x) :
-        fx = self.actfn( self.featx(x) )
+    def forward(self, x, a):
+        fx = self.actfn(self.featx(x))
         # batch x 400
-
-        return fx
-
-    def forward(self, x,a) :
-        fx = self.features(x)
-        # batch x 400
-        concat = torch.cat([ fx, a], dim=1)
+        concat = torch.cat([fx, a], dim=1)
         # batch x 400+self.action_dim
-        out = self.actfn( self.critic1( concat ) )
+        out = self.actfn(self.critic1(concat))
         # batch x 300
         out = self.critic2(out)
         # batch x 1
         return out
 
     def clone(self):
-        cloned = CriticNN(state_dim=self.state_dim,action_dim=self.action_dim,CNN=self.CNN,HER=self.HER,actfn=self.actfn, use_cuda=self.use_cuda)
-        cloned.load_state_dict( self.state_dict() )
+        cloned = CriticNN(state_dim=self.state_dim, action_dim=self.action_dim, CNN=self.CNN, HER=self.HER, actfn=self.actfn, use_cuda=self.use_cuda)
+        cloned.load_state_dict(self.state_dict())
         return cloned
