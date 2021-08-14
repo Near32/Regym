fixed issue with backpropagation through agent's core of the auxiliary losses modules. + added a multi reconstruction module.

benchmark/R2D2/CoMaze/rl_hiddenstate_policy.py
@@ -48,8 +48,11 @@ class RLHiddenStatePolicy(MessagePolicy):
         self.secretgoalStr2id = {"RED":0, "YELLOW":1, "BLUE":2, "GREEN":3}
 
     
-    def get_hiddens(self, info=None):
-        rnn_states = self.model.get_rnn_states()
+    def get_hiddens(self, info=None, from_pred=None):
+        if from_pred is None:
+            rnn_states = self.model.get_rnn_states()
+        else:
+            rnn_states = from_pred['next_rnn_states']
         # Extract 'hidden''s list:
         hiddens = extract_subtrees(in_dict=rnn_states, node_id='hidden')
         # List[List[Tensor]]
@@ -101,7 +104,7 @@ class RLHiddenStatePolicy(MessagePolicy):
                 else:
                     extra = torch.zeros((1,4*3+4*2))
                 extras.append(extra)
-            extras = torch.cat(extras, dim=0)
+            extras = torch.cat(extras, dim=0).to(hiddens.device)
             hiddens = torch.cat([hiddens, extras], dim=1)
             # batch_size x (nbr_parts*hidden_dims + extra_dim)
 
@@ -149,10 +152,11 @@ class RLHiddenStatePolicy(MessagePolicy):
             we either marginalized over possible actions or not.
         """
 
-        log_p_a = self.model.query_action(**x)
+        #log_p_a = self.model.query_action(**x)
+        pred_dict = self.model.query_action(**x)
         # batch_size x action_space_dim
 
-        hiddens = self.get_hiddens(info=x.get('infos', None))
+        hiddens = self.get_hiddens(info=x.get('infos', None), from_pred=pred_dict)
         # batch_size x nbr_parts*hidden_dims + extra_dim if self.augmented
 
         return hiddens

benchmark/R2D2/SymbolicBehaviourBenchmark/benchmark_selfplay_s2b.py
@@ -34,7 +34,7 @@ import ray
 from regym.modules import EnvironmentModule, CurrentAgentsModule
 from regym.modules import MARLEnvironmentModule, RLAgentModule
 
-from regym.modules import ReconstructionFromHiddenStateModule
+from regym.modules import ReconstructionFromHiddenStateModule, MultiReconstructionFromHiddenStateModule
 from rl_hiddenstate_policy import RLHiddenStatePolicy
 
 #from regym.modules import MessageTrajectoryMutualInformationMetricModule
@@ -55,6 +55,7 @@ def make_rl_pubsubmanager(
     save_path=None,
     speaker_rec=False,
     listener_rec=False,
+    listener_comm_rec=False,
     ):
     """
     Create a PubSubManager.
@@ -69,8 +70,11 @@ def make_rl_pubsubmanager(
 
     """
     pipelined = False
+    use_multi_rec = False
     if len(sys.argv) > 2:
       pipelined = any(['pipelined' in arg for arg in sys.argv])
+    if len(sys.argv) >2:
+        use_multi_rec = any(['multi_rec' in arg for arg in sys.argv])
 
     modules = config.pop("modules")
 
@@ -150,6 +154,22 @@ def make_rl_pubsubmanager(
         for exp in traj[player_id]:
             labels.append(torch.from_numpy((1+exp[0])*0.5))
         return labels
+    def build_comm_to_reconstruct_from_trajectory_fn(
+        traj: List[List[Any]],
+        player_id:int,
+        ) -> List[torch.Tensor]:
+        likelihoods = []
+        previous_com = None
+        for exp in traj[player_id]:
+            current_com = torch.from_numpy(exp[-1]['communication_channel'])
+            if previous_com is None:    previous_com = current_com
+
+            target_pred = torch.cat([previous_com, current_com], dim=-1)
+            likelihoods.append(target_pred)
+
+            previous_com = current_com
+        return likelihoods
+
 
     rec_p0_id = "Reconstruction_player0"
     rec_p0_input_stream_ids = {
@@ -183,7 +203,7 @@ def make_rl_pubsubmanager(
       "build_signal_to_reconstruct_from_trajectory_fn": build_signal_to_reconstruct_from_trajectory_fn,
     }
     
-    if speaker_rec:
+    if speaker_rec and not(use_multi_rec):
       modules[rec_p0_id] = ReconstructionFromHiddenStateModule(
         id=rec_p0_id,
         config=rec_p0_config,
@@ -222,12 +242,107 @@ def make_rl_pubsubmanager(
       "build_signal_to_reconstruct_from_trajectory_fn": build_signal_to_reconstruct_from_trajectory_fn,
     }
     
-    if listener_rec:
+    if listener_rec and not(use_multi_rec):
       modules[rec_p1_id] = ReconstructionFromHiddenStateModule(
         id=rec_p1_id,
         config=rec_p1_config,
         input_stream_ids=rec_p1_input_stream_ids,
       )
+    
+    comm_rec_p1_id = "CommReconstruction_player1"
+    comm_rec_p1_input_stream_ids = {
+      "logs_dict":"logs_dict",
+      "losses_dict":"losses_dict",
+      "epoch":"signals:epoch",
+      "mode":"signals:mode",
+
+      "trajectories":f"modules:{envm_id}:trajectories",
+      "filtering_signal":f"modules:{envm_id}:new_trajectories_published",
+
+      "current_agents":"modules:current_agents:ref",  
+    }
+
+    listener_comm_rec_biasing = False 
+    if len(sys.argv) > 2:
+      listener_comm_rec_biasing = any(['listener_comm_rec_biasing' in arg for arg in sys.argv[2:]])
+
+    if listener_comm_rec_biasing:
+      print("WARNING: Biasing for Listener's Communication Reconstruction.")
+    else:
+      print("WARNING: NOT biasing Listener's Communication Reconstruction.")
+    
+    def comm_accuracy_pre_process_fn(
+        pred:torch.Tensor, 
+        target:torch.Tensor,
+        ):
+        # Reshape into (sentence_length, vocab_size):
+        target = target.reshape(-1, 7)
+        pred = pred.reshape(-1, 7)
+        
+        # Retrieve target idx:
+        target_idx = target.max(dim=-1, keepdim=True)[1]
+        
+        pred_distr = pred.softmax(dim=-1)
+
+        #acc = ((pred-0.1<=target).float()+(pred+0.1>=target).float())==2).gather(
+        '''
+        acc = (pred_distr>=0.5).float().gather(
+            dim=-1,
+            index=target_idx,
+        )
+        '''
+        pred_idx = pred.max(dim=-1, keepdim=True)[1]
+        acc = (target_idx == pred_idx).float().reshape(1,-1)
+        # (1, sentence_length)
+        
+        return acc
+
+    comm_rec_p1_config = {
+      "biasing":listener_comm_rec_biasing,
+      "nbr_players":len(agents),
+      "player_id":1,
+      'use_cuda':True,
+      "signal_to_reconstruct_dim": 7*2,
+      "hiddenstate_policy": RLHiddenStatePolicy(agent=agents[-1]),
+      "build_signal_to_reconstruct_from_trajectory_fn": build_comm_to_reconstruct_from_trajectory_fn,
+      "accuracy_pre_process_fn":comm_accuracy_pre_process_fn,
+    }
+    
+    if listener_comm_rec and not(use_multi_rec):
+        modules[comm_rec_p1_id] = ReconstructionFromHiddenStateModule(
+            id=comm_rec_p1_id,
+            config=comm_rec_p1_config,
+            input_stream_ids=comm_rec_p1_input_stream_ids,
+        )
+    
+    if use_multi_rec:
+        if speaker_rec:
+            raise NotImplementedError
+        multi_rec_p1_id = 'multi_rec_p1'
+        rec_dicts = {}
+        rec_p1_config = {
+            "signal_to_reconstruct_dim":4*3,
+            "build_signal_to_reconstruct_from_trajectory_fn":build_signal_to_reconstruct_from_trajectory_fn,
+        }
+        rec_dicts[rec_p1_id] = rec_p1_config
+        comm_rec_p1_config = {
+            "signal_to_reconstruct_dim": 7*2,
+            "build_signal_to_reconstruct_from_trajectory_fn": build_comm_to_reconstruct_from_trajectory_fn,
+            "accuracy_pre_process_fn":comm_accuracy_pre_process_fn,
+        }
+        rec_dicts[comm_rec_p1_id] = comm_rec_p1_config
+        modules[multi_rec_p1_id] = MultiReconstructionFromHiddenStateModule(
+            id=multi_rec_p1_id,
+            config={
+                "biasing":listener_rec_biasing or listener_comm_rec_biasing,
+                "nbr_players":len(agents),
+                "player_id":1,
+                "use_cuda":True,
+                "hiddenstate_policy":RLHiddenStatePolicy(agent=agents[-1]),
+                "rec_dicts": rec_dicts,
+            },
+            input_stream_ids=rec_p1_input_stream_ids,
+        )
 
 
     pipelines = config.pop("pipelines")
@@ -238,11 +353,16 @@ def make_rl_pubsubmanager(
     if pipelined:
       for rlam_id in rlam_ids:
         pipelines['rl_loop_0'].append(rlam_id)
-    if speaker_rec:
-      pipelines["rl_loop_0"].append(rec_p0_id)
-    if listener_rec:
-      pipelines["rl_loop_0"].append(rec_p1_id)
-    
+    if use_multi_rec and (listener_rec or listener_comm_rec):
+        pipelines["rl_loop_0"].append(multi_rec_p1_id)
+    else:
+        if speaker_rec:
+            pipelines["rl_loop_0"].append(rec_p0_id)
+        if listener_rec:
+            pipelines["rl_loop_0"].append(rec_p1_id)
+        if listener_comm_rec:
+            pipelines["rl_loop_0"].append(comm_rec_p1_id)
+     
  
 
     optim_id = "global_optim"
@@ -331,6 +451,7 @@ def train_and_evaluate(agents: List[object],
                        otherplay=False,
                        speaker_rec=False,
                        listener_rec=False,
+                       listener_comm_rec=False,
                        ):
     pubsub = False
     if len(sys.argv) > 2:
@@ -368,6 +489,7 @@ def train_and_evaluate(agents: List[object],
         config=config,
         speaker_rec=speaker_rec,
         listener_rec=listener_rec,
+        listener_comm_rec=listener_comm_rec,
         logger=sum_writer,
       )
 
@@ -449,17 +571,21 @@ def training_process(agent_config: Dict,
     pubsub = False
     speaker_rec = False
     listener_rec = False
+    listener_comm_rec = False
     speaker_rec_biasing = False
     listener_rec_biasing = False
+    listener_comm_rec_biasing = False
     if len(sys.argv) > 2:
       pubsub = any(['pubsub' in arg for arg in sys.argv])
       test_only = any(['test_only' in arg for arg in sys.argv])
       
       speaker_rec = any(['use_speaker_rec' in arg for arg in sys.argv[2:]])
       listener_rec = any(['use_listener_rec' in arg for arg in sys.argv[2:]])
+      listener_comm_rec = any(['use_listener_comm_rec' in arg for arg in sys.argv[2:]])
        
       speaker_rec_biasing = any(['speaker_rec_biasing' in arg for arg in sys.argv[2:]])
       listener_rec_biasing = any(['listener_rec_biasing' in arg for arg in sys.argv[2:]])
+      listener_comm_rec_biasing = any(['listener_comm_rec_biasing' in arg for arg in sys.argv[2:]])
       
       override_seed_argv_idx = [idx for idx, arg in enumerate(sys.argv) if '--seed' in arg]
       if len(override_seed_argv_idx):
@@ -497,7 +623,9 @@ def training_process(agent_config: Dict,
     if speaker_rec:
       base_path = os.path.join(base_path,f"SpeakerReconstruction{'+Biasing-1p3' if speaker_rec_biasing else ''}-BigArch")
     if listener_rec:
-      base_path = os.path.join(base_path,f"ListenerReconstruction{'+Biasing-1p3' if listener_rec_biasing else ''}-BigArch")
+      base_path = os.path.join(base_path,f"ListenerReconstruction{'+Biasing-1p0' if listener_rec_biasing else ''}-BigArch")
+    if listener_comm_rec:
+      base_path = os.path.join(base_path,f"ListenerCommunicationChannelReconstruction{'+Biasing-1p0' if listener_comm_rec_biasing else ''}-BigArch")
      
   
     if task_config["otherplay"]:
@@ -514,6 +642,8 @@ def training_process(agent_config: Dict,
     if not os.path.exists(base_path): os.makedirs(base_path)
 
     task_config['final_path'] = base_path
+    task_config['command_line'] = ' '.join(sys.argv)
+    print(task_config['command_line'])
     yaml.dump(
       task_config, 
       open(
@@ -631,6 +761,7 @@ def training_process(agent_config: Dict,
       otherplay=task_config.get("otherplay", False),
       speaker_rec=speaker_rec,
       listener_rec=listener_rec,
+      listener_comm_rec=listener_comm_rec,
     )
 
     return trained_agents, task 

regym/modules/comaze_goal_ordering_prediction_module.py
@@ -130,11 +130,12 @@ class CoMazeGoalOrderingPredictionModule(Module):
         
         filtering_signal = input_streams_dict["filtering_signal"]
         trajectories = input_streams_dict["trajectories"]
-        compute = True 
+        #compute = True
+        compute = True #(epoch >= 50)
 
         self.iteration += 1
         #if (compute and np.random.random() < 1.0/self.sampling_period) or filtering_signal:
-        if (compute and (self.iteration % self.sampling_period) == 0) or filtering_signal:
+        if compute and (((self.iteration % self.sampling_period) == 0) or filtering_signal):
             if filtering_signal:
                 self.actions = [
                     [
@@ -219,7 +220,7 @@ class CoMazeGoalOrderingPredictionModule(Module):
             logs_dict[f"{mode}/{self.id}/RulesPrediction-Q1/{'Eval' if filtering_signal else 'Sample'}"] = q1_correct_rp.mean()
 
             losses_dict = input_streams_dict["losses_dict"]
-            losses_dict[f"{mode}/{self.id}/GoalOrderingPredictionLoss/{'Eval' if filtering_signal else 'Sample'}"] = [1.0, L_gop]
+            #losses_dict[f"{mode}/{self.id}/GoalOrderingPredictionLoss/{'Eval' if filtering_signal else 'Sample'}"] = [1.0, L_gop]
             losses_dict[f"{mode}/{self.id}/RulesPredictionLoss/{'Eval' if filtering_signal else 'Sample'}"] = [1.0, L_rp]
         
         return outputs_stream_dict

regym/modules/multi_reconstruction_from_hidden_state_module.py
@@ -9,19 +9,19 @@ import copy
 
 from .module import Module
 
-def build_ReconstructionFromHiddenStateModule(
+def build_MultiReconstructionFromHiddenStateModule(
     id:str,
     config:Dict[str,object],
     input_stream_ids:Dict[str,str]=None
     ) -> Module:
-    return ReconstructionFromHiddenStateModule(
+    return MultiReconstructionFromHiddenStateModule(
         id=id,
         config=config, 
         input_stream_ids=input_stream_ids
     )
 
 
-class ReconstructionFromHiddenStateModule(Module):
+class MultiReconstructionFromHiddenStateModule(Module):
     def __init__(
         self,
         id:str,
@@ -53,9 +53,9 @@ class ReconstructionFromHiddenStateModule(Module):
                 if default_id not in input_stream_ids:
                     input_stream_ids[default_id] = default_stream
 
-        super(ReconstructionFromHiddenStateModule, self).__init__(
+        super(MultiReconstructionFromHiddenStateModule, self).__init__(
             id=id,
-            type="ReconstructionFromHiddenStateModule",
+            type="MultiReconstructionFromHiddenStateModule",
             config=config,
             input_stream_ids=input_stream_ids
         )
@@ -68,18 +68,26 @@ class ReconstructionFromHiddenStateModule(Module):
         self.sampling_fraction = 5
         self.sampling_period = 10.0
         
-        if "build_signal_to_reconstruct_from_trajectory_fn" in self.config:
-            self.build_signal_to_reconstruct_from_trajectory_fn = self.config["build_signal_to_reconstruct_from_trajectory_fn"]
-        self.criterion = torch.nn.BCEWithLogitsLoss(reduction='none')
-        self.signal_to_reconstruct_dim = self.config["signal_to_reconstruct_dim"]
         self.hiddenstate_policy = self.config["hiddenstate_policy"]
         self.hidden_state_dim = self.hiddenstate_policy.get_hidden_state_dim()
-        self.prediction_net = [
-            nn.Linear(self.hidden_state_dim, 512),
-            nn.ReLU(),
-            nn.Linear(512, self.signal_to_reconstruct_dim),
-        ]
-        self.prediction_net = nn.Sequential(*self.prediction_net)
+        
+        self.nbr_rec = len(self.config['rec_dicts'])
+        self.build_signal_to_reconstruct_from_trajectory_fn = {}
+        self.criterion = {}
+        self.signal_to_reconstruct_dim = {}
+        self.prediction_net = nn.ModuleList()
+        for rec_name, rec_dict in self.config['rec_dicts'].items():
+            if "build_signal_to_reconstruct_from_trajectory_fn" in rec_dict:
+                self.build_signal_to_reconstruct_from_trajectory_fn[rec_name] = rec_dict["build_signal_to_reconstruct_from_trajectory_fn"]
+            self.criterion[rec_name] = torch.nn.BCEWithLogitsLoss(reduction='none')
+            self.signal_to_reconstruct_dim[rec_name] = rec_dict["signal_to_reconstruct_dim"]
+        
+            prediction_net = [
+                nn.Linear(self.hidden_state_dim, 512),
+                nn.ReLU(),
+                nn.Linear(512, self.signal_to_reconstruct_dim[rec_name]),
+            ]
+            self.prediction_net.append(nn.Sequential(*prediction_net))
 
         if self.config['use_cuda']:
             self = self.cuda()
@@ -92,7 +100,7 @@ class ReconstructionFromHiddenStateModule(Module):
     def compute_reconstruction_loss(
         self,
         x:List[List[Any]],
-        y:List[List[torch.Tensor]],
+        y:Dict[str, List[List[torch.Tensor]]],
         mask:List[List[Any]]=None,
         biasing:bool=False,
     ) -> Dict[str,torch.Tensor]:
@@ -107,7 +115,8 @@ class ReconstructionFromHiddenStateModule(Module):
             expected argument to the :attr hiddenstate_policy:.
         
         :param y: 
-            List[List[torch.Tensor]] containing, for each actor, at each time step t an object
+            Dict[str, List[List[torch.Tensor]]] where each entry corresponds to a reconstruction entry name,
+            and the values are containing, for each actor, at each time step t an object
             representing the signal to reconstruct.
             Shape: signal_to_reconstruct_dim.
         
@@ -128,22 +137,13 @@ class ReconstructionFromHiddenStateModule(Module):
         else:
             hiddenstate_policy = self.hiddenstate_policy.clone()
         
-        L_rec = torch.zeros(batch_size)
-        L_mse = torch.zeros(batch_size)
-        per_actor_per_t_per_dim_acc = [[] for _ in range(batch_size)]
-        per_actor_rec_accuracy = torch.zeros(batch_size)
-
-        dataframe_dict = {
-            'actor_id': [],
-            'timestep': [],
-        }
+        L_rec = {rec_name:torch.zeros(batch_size) for rec_name in self.config['rec_dicts']} 
+        L_mse = {rec_name:torch.zeros(batch_size) for rec_name in self.config['rec_dicts']}
+        per_actor_per_t_per_dim_acc = {rec_name:[[] for _ in range(batch_size)] for rec_name in self.config['rec_dicts']} 
+        per_actor_rec_accuracy = {rec_name:torch.zeros(batch_size) for rec_name in self.config['rec_dicts']}
 
         for actor_id in range(batch_size):
             hiddenstate_policy.reset(1)
-            labels_list = y[actor_id]
-            # in range [0,1]
-            # 1xsignal_to_reconstruct_dim
-
             T = len(x[actor_id])
             if mask is None:
                 eff_mask = torch.ones((batch_size, T))
@@ -152,7 +152,7 @@ class ReconstructionFromHiddenStateModule(Module):
 
             for t in range(T):
                 m = eff_mask[actor_id][t] 
-                labels = labels_list[t]
+                
                 if biasing:
                     hs_t = hiddenstate_policy(x[actor_id][t])
                     # 1 x hidden_state_dim
@@ -161,44 +161,46 @@ class ReconstructionFromHiddenStateModule(Module):
                         hs_t = hiddenstate_policy(x[actor_id][t]).detach()
                     # 1 x hidden_state_dim
                 
-                logit_pred = self.prediction_net(hs_t.reshape(1,-1))
-
                 m = m.to(hs_t.device)
-                if labels.device != logit_pred.device: labels = labels.to(logit_pred.device)    
-                ###                
-                pred = torch.sigmoid(logit_pred)
-                # 1x dim
-                if 'accuracy_pre_process_fn' not in self.config:
-                    per_dim_acc_t = (((pred-5e-2<=labels).float()+(pred+5e-2>=labels)).float()>=2).float()
-                else:
-                    per_dim_acc_t = self.config['accuracy_pre_process_fn'](pred=pred, target=labels)
-                # 1x dim
-                per_actor_per_t_per_dim_acc[actor_id].append(per_dim_acc_t)
-                ###
-
-                L_rec_t = self.criterion(
-                    input=logit_pred,
-                    target=labels.detach(),
-                ).mean()
-                # 1 
-                L_mse_t = 0.5*torch.pow(pred-labels, 2.0).mean()
-                # 1
-
-                if L_rec.device != L_rec_t.device:    L_rec = L_rec.to(L_rec_t.device)
-                if L_mse.device != L_mse_t.device:    L_mse = L_mse.to(L_mse_t.device)
                 
-                L_rec[actor_id:actor_id+1] += m*L_rec_t.reshape(-1)
-                # batch_size
-                L_mse[actor_id:actor_id+1] += m*L_mse_t.reshape(-1)
+                for rec_idx, rec_name in enumerate(y):
+                    labels = y[rec_name][actor_id][t]
+                    # in range [0,1]
+                    # 1xsignal_to_reconstruct_dim
+                    
+                    logit_pred = self.prediction_net[rec_idx](hs_t.reshape(1,-1))
 
-                dataframe_dict['actor_id'].append(actor_id)
-                dataframe_dict['timestep'].append(t)
+                    if labels.device != logit_pred.device: labels = labels.to(logit_pred.device)    
+                    ###                
+                    pred = torch.sigmoid(logit_pred)
+                    # 1x dim
+                    if 'accuracy_pre_process_fn' not in self.config['rec_dicts'][rec_name]:
+                        per_dim_acc_t = (((pred-5e-2<=labels).float()+(pred+5e-2>=labels)).float()>=2).float()
+                    else:
+                        per_dim_acc_t = self.config['rec_dicts'][rec_name]['accuracy_pre_process_fn'](pred=pred, target=labels)
+                    # 1x dim
+                    per_actor_per_t_per_dim_acc[rec_name][actor_id].append(per_dim_acc_t)
+                    ###
 
-            per_actor_per_t_per_dim_acc[actor_id] = torch.cat(per_actor_per_t_per_dim_acc[actor_id], dim=0)
-            # timesteps x nbr_goal
-            #correct_pred_indices = torch.nonzero((per_actor_per_t_per_dim_acc[actor_id].sum(dim=-1)==self.signal_to_reconstruct_dim).float())
-            #per_actor_rec_accuracy[actor_id] = correct_pred_indices.shape[0]/T*100.0
-            per_actor_rec_accuracy[actor_id] = per_actor_per_t_per_dim_acc[actor_id].mean()*100.0
+                    L_rec_t = self.criterion[rec_name](
+                        input=logit_pred,
+                        target=labels.detach(),
+                    ).mean()
+                    # 1 
+                    L_mse_t = 0.5*torch.pow(pred-labels, 2.0).mean()
+                    # 1
+
+                    if L_rec[rec_name].device != L_rec_t.device:    L_rec[rec_name] = L_rec[rec_name].to(L_rec_t.device)
+                    if L_mse[rec_name].device != L_mse_t.device:    L_mse[rec_name] = L_mse[rec_name].to(L_mse_t.device)
+                
+                    L_rec[rec_name][actor_id:actor_id+1] += m*L_rec_t.reshape(-1)
+                    # batch_size
+                    L_mse[rec_name][actor_id:actor_id+1] += m*L_mse_t.reshape(-1)
+
+            for rec_name in self.config['rec_dicts']:
+                per_actor_per_t_per_dim_acc[rec_name][actor_id] = torch.cat(per_actor_per_t_per_dim_acc[rec_name][actor_id], dim=0)
+                # timesteps x nbr_goal
+                per_actor_rec_accuracy[rec_name][actor_id] = per_actor_per_t_per_dim_acc[rec_name][actor_id].mean()*100.0
             ###
         
         if biasing:
@@ -255,16 +257,15 @@ class ReconstructionFromHiddenStateModule(Module):
                 ]
                 # (batch_size, timestep, keys:values) 
                 
-                if hasattr(self,'build_signal_to_reconstruct_from_trajectory_fn'):
-                    self.labels = [ 
-                        self.build_signal_to_reconstruct_from_trajectory_fn(
+                self.labels = {}
+                for rec_idx, rec_name in enumerate(self.config['rec_dicts']):
+                    self.labels[rec_name] = [ 
+                        self.build_signal_to_reconstruct_from_trajectory_fn[rec_name](
                             traj=traj,
                             player_id=self.player_id,
                         )
                         for traj in trajectories
                     ]
-                else:
-                    raise NotImplementedError
 
                 x = self.x 
                 labels = self.labels
@@ -278,33 +279,37 @@ class ReconstructionFromHiddenStateModule(Module):
                 indices = np.random.choice(list(range(len(self.x))), size=len(self.x)//self.sampling_fraction, replace=False)
             
             x = [traj for idx, traj in enumerate(self.x) if idx in indices]
-            labels = [labels for idx, labels in enumerate(self.labels) if idx in indices]
+            labels_dict = {}
+            for rec_name in self.labels:
+                labels_dict[rec_name] = [labels for idx, labels in enumerate(self.labels[rec_name]) if idx in indices]
 
             mask = None
             
             ## Measure:
             output_dict = self.compute_reconstruction_loss(
                 x=x, 
-                y=labels,
+                y=labels_dict,
                 mask=mask,
                 biasing=self.biasing,
             )
             
             L_rec = output_dict['l_rec']
-            # batch_size
+            # dict nbr_rec x batch_size
             rec_accuracy = output_dict['per_actor_rec_accuracy']
-            # batch_size
+            # dict nbr_rec x batch_size
 
             L_mse = output_dict['l_mse']
-
-            logs_dict[f"{mode}/{self.id}/ReconstructionAccuracy/{'Eval' if filtering_signal else 'Sample'}"] = rec_accuracy.mean()
-            #logs_dict[f"{mode}/{self.id}/ReconstructionMSELoss/{'Eval' if filtering_signal else 'Sample'}"] = L_mse.mean()
-            logs_dict[f"{mode}/{self.id}/ReconstructionLoss/Log/BCE/{'Eval' if filtering_signal else 'Sample'}"] = L_rec.mean()
-
-
+            
             losses_dict = input_streams_dict["losses_dict"]
-            #losses_dict[f"{mode}/{self.id}/ReconstructionLoss/{'Eval' if filtering_signal else 'Sample'}"] = [1.0, L_rec]
-            losses_dict[f"{mode}/{self.id}/ReconstructionLoss/MSE/{'Eval' if filtering_signal else 'Sample'}"] = [1.0, L_mse]
+            
+            for rec_name in self.config['rec_dicts']:
+                logs_dict[f"{mode}/{self.id}/{rec_name}/ReconstructionAccuracy/{'Eval' if filtering_signal else 'Sample'}"] = rec_accuracy[rec_name].mean()
+                #logs_dict[f"{mode}/{self.id}/{rec_name}/ReconstructionMSELoss/{'Eval' if filtering_signal else 'Sample'}"] = L_mse[rec_name].mean()
+                logs_dict[f"{mode}/{self.id}/{rec_name}/ReconstructionLoss/Log/BCE/{'Eval' if filtering_signal else 'Sample'}"] = L_rec[rec_name].mean()
+
+
+                #losses_dict[f"{mode}/{self.id}/{rec_name}/ReconstructionLoss/{'Eval' if filtering_signal else 'Sample'}"] = [1.0, L_rec[rec_name]]
+                losses_dict[f"{mode}/{self.id}/{rec_name}/ReconstructionLoss/MSE/{'Eval' if filtering_signal else 'Sample'}"] = [1.0, L_mse[rec_name]]
  
         return outputs_stream_dict
     