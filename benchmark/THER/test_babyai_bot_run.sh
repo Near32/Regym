fdf268b06b5a5f1c7900e0ac3055e330e8941a00
Denamganai Kevin <denamganai.kevin@gmail.com>
2023-02-24 22:45:11 +0000

    patching a bug in the BabyAI bot. + BabyAI installation scripts now points at the patched commit in my forked repo. + updated babyai bot module : now scores perfect 100% on Pickup tasks, at least.

--- benchmark/THER/test_babyai_bot_run.sh
+++ benchmark/THER/test_babyai_bot_run.sh
@@ -1,11 +1,11 @@
 WANDB_CACHE_DIR=./wandb_cache/ python -m ipdb -c c benchmark_wandb_ther.py --seed=10 \
 --config=babyAI_wandb_benchmark_OracleTHER_config.yaml \
---n_step=3 --nbr_actor=4 --nbr_minibatches=1 --batch_size=32 \
---min_capacity=1e3 --replay_capacity=2e3 --learning_rate=6.25e-4 \
---sequence_replay_burn_in_ratio=0.5 --weights_entropy_lambda=0.0 \
+--n_step=3 --nbr_actor=32 --nbr_minibatches=4 --batch_size=64 \
+--min_capacity=1e3 --replay_capacity=2e3 --tau=1.0e-3 --learning_rate=6.25e-5 \
+--sequence_replay_burn_in_ratio=0.0 --weights_entropy_lambda=0.0 \
 --adam_weight_decay=0.0 --ther_adam_weight_decay=0.0 \
 --THER_use_THER=True --THER_use_PER=True --THER_observe_achieved_goal=True \
---THER_feedbacks_failure_reward=-10 --THER_feedbacks_success_reward=10 \
+--THER_feedbacks_failure_reward=0 --THER_feedbacks_success_reward=1 \
 --THER_episode_length_reward_shaping=True \
 --THER_replay_capacity=1e2 --THER_min_capacity=12 \
 --THER_predictor_nbr_minibatches=1 --THER_predictor_batch_size=32 \
@@ -16,7 +16,7 @@ WANDB_CACHE_DIR=./wandb_cache/ python -m ipdb -c c benchmark_wandb_ther.py --see
 --THER_predictor_accuracy_safe_to_relabel_threshold=0.2 --THER_filter_predicate_fn=False \
 --THER_relabel_terminal=False --THER_filter_out_timed_out_episode=True \
 --THER_train_contrastively=False --THER_contrastive_training_nbr_neg_examples=0 \
---single_pick_episode=True --THER_timing_out_episode_length_threshold=40 \
+--single_pick_episode=False --THER_timing_out_episode_length_threshold=40 \
 --BabyAI_Bot_action_override=True \
 --train_observation_budget=2.0e7
 
