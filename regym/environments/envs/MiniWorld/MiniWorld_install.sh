ea634fea5e8ff61d5c8eca47ba27b4e2f91c6e12
Kevin Denamganai <denamganai.kevin@gmail.com>
2023-03-30 15:23:56 +0100

    adding MiniWorld, with ConditionalPickUpObject environment.

---
+++ regym/environments/envs/MiniWorld/MiniWorld_install.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Near32/Miniworld.git
+# Installation from a fork of the original repo.
+# The fork adds a ConditionalPickUpObject environment...
+cd Miniworld
+git checkout develop
+pip install -e .
