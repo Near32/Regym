21ae8564679735b31bc9d664b3643d8b610e32f6
Kevin Denamganai <denamganai.kevin@gmail.com>
2023-05-16 20:29:46 +0100

    ETHER v0.3 with dual semantic co-occurrence grounding.

--- benchmark/ETHER/run_babyai_ether.sh
+++ benchmark/ETHER/run_babyai_ether.sh
@@ -20,7 +20,7 @@ WANDB_CACHE_DIR=./wandb_cache/ python -m ipdb -c c benchmark_wandb_ether.py --se
 --ETHER_listener_based_predicated_reward_fn=False \
 --ETHER_rg_with_semantic_grounding_metric=True \
 --ETHER_rg_homoscedastic_multitasks_loss=False \
---ETHER_rg_use_semantic_cooccurrence_grounding=False \
+--ETHER_rg_use_semantic_cooccurrence_grounding=True \
 --ETHER_rg_semantic_cooccurrence_grounding_lambda=1.0e2 \
 --ETHER_rg_semantic_cooccurrence_grounding_noise_magnitude=0.2 \
 --ETHER_lock_test_storage=True --ETHER_rg_filter_out_non_unique=False\
@@ -28,12 +28,12 @@ WANDB_CACHE_DIR=./wandb_cache/ python -m ipdb -c c benchmark_wandb_ether.py --se
 --ETHER_rg_egocentric=False \
 --ETHER_rg_object_centric_version=2 --ETHER_rg_descriptive_version=1 \
 --ETHER_rg_learning_rate=3e-4 --ETHER_rg_weight_decay=0.0 \
---ETHER_rg_vocab_size=64 --ETHER_rg_training_period=4096 \
+--ETHER_rg_vocab_size=64 --ETHER_rg_training_period=512 \
 --ETHER_rg_descriptive=False --ETHER_rg_object_centric=False \
 --ETHER_rg_use_curriculum_nbr_distractors=False \
---ETHER_rg_nbr_epoch_per_update=8 --ETHER_rg_accuracy_threshold=99 \
+--ETHER_rg_nbr_epoch_per_update=1 --ETHER_rg_accuracy_threshold=99 \
 --ETHER_rg_nbr_train_distractors=31 --ETHER_rg_nbr_test_distractors=7 \
---ETHER_replay_capacity=4096 --ETHER_test_replay_capacity=512 \
+--ETHER_replay_capacity=2048 --ETHER_test_replay_capacity=256 \
 --ETHER_rg_distractor_sampling=uniform \
 --THER_use_PER=True --THER_observe_achieved_goal=False \
 --THER_lock_test_storage=True \
@@ -42,7 +42,7 @@ WANDB_CACHE_DIR=./wandb_cache/ python -m ipdb -c c benchmark_wandb_ether.py --se
 --THER_replay_capacity=1e2 --THER_min_capacity=12 \
 --THER_predictor_nbr_minibatches=1 --THER_predictor_batch_size=32 \
 --THER_predictor_test_train_split_interval=5 --THER_test_replay_capacity=1e2 \
---THER_test_min_capacity=4 --THER_replay_period=1024 \
+--THER_test_min_capacity=4 --THER_replay_period=256 \
 --THER_train_on_success=False --THER_nbr_training_iteration_per_update=128 \
 --THER_predict_PADs=False --THER_predictor_accuracy_threshold=0.95 \
 --THER_predictor_accuracy_safe_to_relabel_threshold=0.0 --THER_filter_predicate_fn=True \
