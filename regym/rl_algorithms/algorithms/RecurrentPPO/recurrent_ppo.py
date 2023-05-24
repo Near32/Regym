RPPO : + solved bug with unroll_length <-> advantage & return computation... (sidx) + solved bug with EpisodicLife on Pong... + added a more sensible layer initialization scheme for the actor critic rl head in Archi.

regym/rl_algorithms/algorithms/RecurrentPPO/recurrent_ppo.py
@@ -312,11 +312,11 @@ class RecurrentPPOAlgorithm(R2D2Algorithm):
         non_terminals = []
 
         for sidx in range(len(self.storages[storage_idx])):
-            r = self.storages[storage_idx].r[0][0][0]
-            v = self.storages[storage_idx].v[0][0][0]
+            r = self.storages[storage_idx].r[0][sidx][0]
+            v = self.storages[storage_idx].v[0][sidx][0]
             # (temporal_dim=unroll_length x 1)
             v_key = 'v'
-            non_terminal = self.storages[storage_idx].non_terminal[0][0][0]#.squeeze().tolist()
+            non_terminal = self.storages[storage_idx].non_terminal[0][sidx][0]#.squeeze().tolist()
             
             rs.append(r)
             vs.append(v)
@@ -328,6 +328,7 @@ class RecurrentPPOAlgorithm(R2D2Algorithm):
         non_terminal = torch.cat(non_terminals, dim=0).squeeze().tolist()
         # (temporal_dim = nbr_storages * unroll_length x 1)
         
+        # sidx contains the last segment of temporally-ordered data
         succ_s = self.storages[storage_idx].succ_s[0][sidx][0]
         rnn_states = self.storages[storage_idx].rnn_states[0][sidx]
 
@@ -410,7 +411,7 @@ class RecurrentPPOAlgorithm(R2D2Algorithm):
             )
             returns = final_prediction[v_key].cpu().detach()
         else:
-            returns = torch.zeros(1,1)
+            returns = r[-1].reshape((1,1)) #torch.zeros(1,1)
         
         # Adding next state return/value and dummy advantages to the storage on the N+1 spots: 
         # not used during optimization, but necessary to compute the returns and advantages of previous states.

regym/rl_algorithms/algorithms/RecurrentPPO/recurrent_ppo_loss.py
@@ -184,22 +184,29 @@ def compute_loss(
     
     #prediction = model(obs=states, action=actions, rnn_states=rnn_states)
     
-    ratio = torch.exp((training_predictions['log_pi_a'] - training_log_probs_old))
+    ratio = torch.exp((training_predictions['log_pi_a'] - training_log_probs_old.detach()))
     
     if kwargs['standardized_adv']:
-      adv = training_std_advantages
+      #adv = training_std_advantages
+      # Standardize on minibatch:
+      def standardize(x):
+          stable_eps = 1.0e-8
+          std_x = (x-x.mean())/(x.std()+stable_eps)
+          return std_x
+      adv = standardize(training_advantages)
     else:
-      adv = training_advantages
+      adv = training_advantages.detach()
 
     adv = adv.reshape((batch_size, training_length))
-    obj = ratio * adv
+    obj = ratio * adv.detach()
     obj_clipped = torch.clamp(ratio,
                               1.0 - kwargs['ppo_ratio_clip'],
-                              1.0 + kwargs['ppo_ratio_clip']) * adv
+                              1.0 + kwargs['ppo_ratio_clip']) * adv.detach()
     
-    policy_val = -torch.min(obj, obj_clipped) #.mean()
+    policy_val = policy_loss = -torch.min(obj, obj_clipped) #.mean()
     entropy_val = training_predictions['ent'] #.mean()
-    policy_loss = policy_val - kwargs['entropy_weight'] * entropy_val 
+
+    #policy_loss = policy_val - kwargs['entropy_weight'] * entropy_val 
     # L^{clip} and L^{S} from original paper
     #policy_loss = -torch.min(obj, obj_clipped).mean() - entropy_weight * prediction['ent'].mean() # L^{clip} and L^{S} from original paper
     
@@ -212,12 +219,14 @@ def compute_loss(
     # TODO: Testing in progress : trying mean then addition to check if it affects anything:
     #total_loss = policy_loss + value_loss
     total_loss = policy_loss.mean(-1) + value_loss.mean(-1)
+    total_loss = policy_loss.mean(-1) + value_loss.mean(-1) - kwargs['entropy_weight']*entropy_val.mean(-1)
     # Mean over unroll_length :
     #total_loss = total_loss.mean(-1)
 
     wandb.log({'Training/RatioMean': ratio.mean().cpu().item(), "training_step": iteration_count}, commit=False)
     #summary_writer.add_histogram('Training/Ratio', ratio.cpu(), iteration_count)
     wandb.log({'Training/AdvantageMean': training_advantages.mean().cpu().item(), "training_step": iteration_count}, commit=False)
+    wandb.log({'Training/AdvantageStd': training_advantages.std().cpu().item(), "training_step": iteration_count}, commit=False)
     #summary_writer.add_histogram('Training/Advantage', advantages.cpu(), iteration_count)
     wandb.log({'Training/MeanVValues': training_predictions['v'].cpu().mean().item(), "training_step": iteration_count}, commit=False)
     wandb.log({'Training/MeanReturns': returns.cpu().mean().item(), "training_step": iteration_count}, commit=False)