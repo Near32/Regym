fb870a09451faf9d5764efad3dba17d0ebfe3d4e
Kevin Denamganai <denamganai.kevin@gmail.com>
2023-08-08 07:44:19 +0100

    caught bug in ReferentialGym. + ETHER hyperparameter finetuning in progress...

--- benchmark/ETHER/room12x5Objs_R2D2+ETHER+SharedObsEncoder_run.sh
+++ benchmark/ETHER/room12x5Objs_R2D2+ETHER+SharedObsEncoder_run.sh
@@ -13,10 +13,10 @@ WANDB_CACHE_DIR=./wandb_cache/ xvfb-run -a -s "-screen 0 1024x768x24 -ac +extens
 --use_HER=False --goal_oriented=False \
 --ETHER_use_ETHER=True --THER_use_THER=True \
 --ETHER_rg_sanity_check_compactness_ambiguity_metric=False \
---ETHER_rg_shared_architecture=False \
+--ETHER_rg_shared_architecture=True \
 --ETHER_rg_with_logits_mdl_principle=True \
---ETHER_rg_logits_mdl_principle_factor=1.0e-3 \
---ETHER_rg_logits_mdl_principle_accuracy_threshold=70.0 \
+--ETHER_rg_logits_mdl_principle_factor=1.0e-4 \
+--ETHER_rg_logits_mdl_principle_accuracy_threshold=20.0 \
 --ETHER_rg_agent_loss_type=Impatient+Hinge \
 --ETHER_use_supervised_training=False \
 --ETHER_use_continuous_feedback=False \
@@ -31,11 +31,11 @@ WANDB_CACHE_DIR=./wandb_cache/ xvfb-run -a -s "-screen 0 1024x768x24 -ac +extens
 --ETHER_rg_egocentric=False \
 --ETHER_rg_object_centric_version=2 --ETHER_rg_descriptive_version=1 \
 --ETHER_rg_learning_rate=6.25e-5 --ETHER_rg_weight_decay=0.0 \
---ETHER_rg_vocab_size=64 --ETHER_rg_training_period=8192 \
+--ETHER_rg_vocab_size=64 --ETHER_rg_training_period=4096 \
 --ETHER_rg_descriptive=False --ETHER_rg_use_curriculum_nbr_distractors=False \
---ETHER_rg_nbr_epoch_per_update=2 --ETHER_rg_accuracy_threshold=99 \
+--ETHER_rg_nbr_epoch_per_update=8 --ETHER_rg_accuracy_threshold=70 \
 --ETHER_rg_nbr_train_distractors=7 --ETHER_rg_nbr_test_distractors=7 \
---ETHER_replay_capacity=8192 --ETHER_test_replay_capacity=2048 \
+--ETHER_replay_capacity=4096 --ETHER_test_replay_capacity=1024 \
 --ETHER_rg_distractor_sampling=uniform \
 --RP_use_PER=True \
 --RP_lock_test_storage=False \
@@ -81,13 +81,13 @@ WANDB_CACHE_DIR=./wandb_cache/ xvfb-run -a -s "-screen 0 1024x768x24 -ac +extens
 --THER_train_on_success=False --THER_nbr_training_iteration_per_update=128 \
 --THER_predict_PADs=False --THER_predictor_accuracy_threshold=0.95 \
 --THER_predictor_accuracy_safe_to_relabel_threshold=0.0 --THER_filter_predicate_fn=True \
---THER_relabel_terminal=False --THER_filter_out_timed_out_episode=True \
---THER_train_contrastively=False --THER_contrastive_training_nbr_neg_examples=2 \
+--THER_relabel_terminal=False --THER_filter_out_timed_out_episode=False \
+--THER_train_contrastively=False --THER_contrastive_training_nbr_neg_examples=0 \
 --THER_timing_out_episode_length_threshold=40 \
 --BabyAI_Bot_action_override=False \
 --n_step=3 --nbr_actor=32 --eps_greedy_alpha=2.0 \
 --nbr_minibatches=1 --batch_size=64 \
---min_capacity=1e3 --replay_capacity=5e3 --learning_rate=6.25e-5 \
+--min_capacity=4e3 --replay_capacity=5e3 --learning_rate=6.25e-5 \
 --sequence_replay_burn_in_ratio=0.5 --weights_entropy_lambda=0.0 \
 --sequence_replay_unroll_length=20 --sequence_replay_overlap_length=10 \
 --sequence_replay_use_online_states=True --sequence_replay_use_zero_initial_states=False \
@@ -99,5 +99,5 @@ WANDB_CACHE_DIR=./wandb_cache/ xvfb-run -a -s "-screen 0 1024x768x24 -ac +extens
 --train_observation_budget=1.0e6
 
 #--train_observation_budget=300000 
-
+# WARNING: if THER_contrastive_training_nbr_neg_examples != 0 then THER_train_contrastively is toggled to True. 
 
