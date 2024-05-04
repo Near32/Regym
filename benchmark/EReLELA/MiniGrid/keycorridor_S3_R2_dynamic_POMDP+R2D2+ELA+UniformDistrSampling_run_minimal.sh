1eab9e158ed7740b5b5024e776a7255afab9be5b
Denamganai Kevin <denamganai.kevin@gmail.com>
2024-05-04 12:30:26 +0100

    sync

--- benchmark/EReLELA/MiniGrid/keycorridor_S3_R2_dynamic_POMDP+R2D2+ELA+UniformDistrSampling_run_minimal.sh
+++ benchmark/EReLELA/MiniGrid/keycorridor_S3_R2_dynamic_POMDP+R2D2+ELA+UniformDistrSampling_run_minimal.sh
@@ -1,5 +1,5 @@
 WANDB_CACHE_DIR=./wandb_cache/ xvfb-run -a -s "-screen 0 1024x768x24 -ac +extension GLX +render -noreset" python -m ipdb -c c ../benchmark_wandb_erelela.py \
---seed=30 --env_seed=12 --static_envs=False \
+--seed=10 --env_seed=12 --static_envs=False \
 --with_early_stopping=False \
 --use_cuda=True \
 --project=EReLELA-KeyCorridor-Benchmark \
