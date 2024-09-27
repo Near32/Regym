2eb9c68add6ae031c5e4a59b9d63f0e61d1a84b0
Denamganai Kevin <denamganai.kevin@gmail.com>
2024-09-27 10:38:30 +0100

    sync

--- benchmark/EReLELA/MiniGrid/test_KC-S3R3-GPU-usage.sh
+++ benchmark/EReLELA/MiniGrid/test_KC-S3R3-GPU-usage.sh
@@ -1,5 +1,5 @@
-WANDB_CACHE_DIR=./wandb_cache/ xvfb-run -a -s "-screen 0 1024x768x24 -ac +extension GLX +render -noreset" python -m ipdb -c c ../benchmark_wandb_erelela.py \
---seed=20 --env_seed=12 --static_envs=False \
+WANDB_CACHE_DIR=./wandb_cache/ WANDB_DATA_DIR=./wandb_data_dir/ xvfb-run -a -s "-screen 0 1024x768x24 -ac +extension GLX +render -noreset" python -m ipdb -c c ../benchmark_wandb_erelela.py \
+--seed=10 --env_seed=12 --static_envs=False \
 --with_early_stopping=False \
 --use_cuda=True \
 --project=EReLELA-KeyCorridor-Benchmark \
