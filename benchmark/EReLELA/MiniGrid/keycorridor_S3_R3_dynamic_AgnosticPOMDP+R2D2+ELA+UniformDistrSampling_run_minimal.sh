8e5a6d90339e69c5c9225207d002e496ae787f4f
Denamganai Kevin <denamganai.kevin@gmail.com>
2024-11-23 13:09:47 +0000

    ERELELA: KC-S3-R3 rg acc=90 + test distr : 31->3

--- benchmark/EReLELA/MiniGrid/keycorridor_S3_R3_dynamic_AgnosticPOMDP+R2D2+ELA+UniformDistrSampling_run_minimal.sh
+++ benchmark/EReLELA/MiniGrid/keycorridor_S3_R3_dynamic_AgnosticPOMDP+R2D2+ELA+UniformDistrSampling_run_minimal.sh
@@ -62,8 +62,8 @@ WANDB_CACHE_DIR=./wandb_cache/ WANDB_DATA_DIR=./wandb_data_dir/ xvfb-run -a -s "
 --ELA_rg_training_max_skip=32 \
 --ELA_rg_training_adaptive_period=False \
 --ELA_rg_descriptive=True --ELA_rg_use_curriculum_nbr_distractors=False \
---ELA_rg_nbr_epoch_per_update=32 --ELA_rg_accuracy_threshold=80 \
---ELA_rg_nbr_train_distractors=256 --ELA_rg_nbr_test_distractors=31 \
+--ELA_rg_nbr_epoch_per_update=32 --ELA_rg_accuracy_threshold=90 \
+--ELA_rg_nbr_train_distractors=256 --ELA_rg_nbr_test_distractors=3 \
 --ELA_replay_capacity=8192 --ELA_test_replay_capacity=2048 \
 --ELA_rg_distractor_sampling_scheme_version=2 \
 --ELA_rg_distractor_sampling_with_replacement=True \
