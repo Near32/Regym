393ee89658ef3b0da9172dcbb6b3d5176b81a1c0
Kevin Denamganai <denamganai.kevin@gmail.com>
2025-09-21 21:57:29 +0000

    adding KC-S3R3 RPPO+AT script and config.

--- benchmark/EReLELA/MiniGrid/keycorridor_S3_R3_symbolic_dynamic_RIDE_AgnosticPOMDP+RPPO+ELA+AT+UniformDistrSampling_run_minimal.sh
+++ benchmark/EReLELA/MiniGrid/keycorridor_S3_R3_symbolic_dynamic_RIDE_AgnosticPOMDP+RPPO+ELA+AT+UniformDistrSampling_run_minimal.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 #--config=keycorridor_S3_R3_minigrid_wandb_benchmark_AgnosticPOMDPERELELA_config.yaml \
 WANDB_CACHE_DIR=./wandb_cache/ WANDB_DATA_DIR=./wandb_data_dir/ xvfb-run -a -s "-screen 0 1024x768x24 -ac +extension GLX +render -noreset" python -m ipdb -c c ../benchmark_wandb_erelela.py \
---seed=110 --env_seed=12 --static_envs=False \
+--seed=100 --env_seed=12 --static_envs=False \
 --with_early_stopping=False \
 --use_cuda=True \
---project=EReLELA+AT+R2D2-KeyCorridor-Symbolic-Benchmark \
+--project=EReLELA+AT+RPPO-KeyCorridor-Symbolic-Benchmark \
 --success_threshold=0.01 \
---config=keycorridor_S3_R3_symbolic_minigrid_wandb_RIDE_benchmark_AgnosticPOMDPERELELA+R2D2_config.yaml \
+--config=keycorridor_S3_R3_symbolic_minigrid_wandb_RIDE_benchmark_AgnosticPOMDPERELELA+RPPO_config.yaml \
 --language_guided_curiosity=False \
 --language_guided_curiosity_descr_type='descr' \
 --language_guided_curiosity_extrinsic_weight=10.0 \
@@ -59,7 +59,7 @@ WANDB_CACHE_DIR=./wandb_cache/ WANDB_DATA_DIR=./wandb_data_dir/ xvfb-run -a -s "
 --ELA_rg_object_centric_version=2 --ELA_rg_descriptive_version=1 \
 --ELA_rg_learning_rate=3e-4 --ELA_rg_weight_decay=0.0 \
 --ELA_rg_l1_weight_decay=0.0 --ELA_rg_l2_weight_decay=0.0 \
---ELA_rg_vocab_size=64 --ELA_rg_max_sentence_length=4 \
+--ELA_rg_vocab_size=64 --ELA_rg_max_sentence_length=128 \
 --ELA_rg_training_period=256000 \
 --ELA_rg_training_max_skip=32 \
 --ELA_rg_training_adaptive_period=False \
@@ -81,11 +81,11 @@ WANDB_CACHE_DIR=./wandb_cache/ WANDB_DATA_DIR=./wandb_data_dir/ xvfb-run -a -s "
 --n_step=3 --nbr_actor=32 \
 --epsstart=1.0 --epsend=0.1 \
 --epsdecay=1000000 --eps_greedy_alpha=2.0 \
---nbr_minibatches=1 --batch_size=64 \
+--nbr_minibatches=1 --batch_size=32 \
 --min_capacity=4e3 --min_handled_experiences=28e3 --replay_capacity=20e3 \
---learning_rate=6.25e-5 \
+--learning_rate=1.0e-4 \
 --sequence_replay_burn_in_ratio=0.5 --weights_entropy_lambda=0.0 \
---sequence_replay_unroll_length=20 --sequence_replay_overlap_length=10 \
+--sequence_replay_unroll_length=128 --sequence_replay_overlap_length=0 \
 --sequence_replay_use_online_states=True --sequence_replay_use_zero_initial_states=False \
 --sequence_replay_store_on_terminal=False --HER_target_clamping=False \
 --adam_weight_decay=0.0 --ther_adam_weight_decay=0.0 \
@@ -105,7 +105,7 @@ WANDB_CACHE_DIR=./wandb_cache/ WANDB_DATA_DIR=./wandb_data_dir/ xvfb-run -a -s "
 --mini_batch_size=32 \
 --gradient_clip=5.0 \
 --value_weight=0.5 \
---entropy_weight=0.01 \
+--entropy_weight=0.1 \
 --time_limit=0 \
 --benchmarking_record_episode_interval=10000000 \
 --benchmarking_interval=1.0e5 \
